57e0339e69973bc1b161d32cdb58d016a208b9ad
Danil Ermolaev <danilssau6364@yandex.ru>
2025-05-25 21:40:40 +0400

    8 version

--- modules/connection.py
+++ modules/connection.py
@@ -2,12 +2,13 @@ from clickhouse_connect import get_client
 from airflow.models import Variable
 
 def get_clickhouse_client():
-    """Возвращает подключение к ClickHouse через HTTP, используя переменные Airflow"""
+    """Возвращает подключение к ClickHouse используя переменные Airflow (через clickhouse-connect с отключенной SSL-проверкой)"""
     return get_client(
         host=Variable.get("CLICKHOUSE_HOST"),
-        username=Variable.get("CLICKHOUSE_USER", default_var='default'),
+        user=Variable.get("CLICKHOUSE_USER", default_var='default'),
         password=Variable.get("CLICKHOUSE_PASSWORD", default_var=''),
         database=Variable.get("CLICKHOUSE_DB", default_var='default'),
         port=int(Variable.get("CLICKHOUSE_PORT", default_var='8443')),
-        secure=True  # Включает HTTPS
+        secure=True,
+        verify=False  # <-- отключаем проверку SSL
     )
